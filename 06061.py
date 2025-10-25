915fe9bd0c5d13b16549d27cfb0e18389a3cfb71
lavien12345 <cookie.lu66@gmial.com>
2025-10-25 16:40:35 +0800

    Initial commit: full Flask project with MySQL backup

---
+++ 06061.py
@@ -0,0 +1,19 @@
+def input_dic(children):
+    for _ in range(5):
+        name=input("Enter the children's name: ")
+        age=input("Enter the children's age: ")
+        children[name]=age
+def print_dic(children):
+    for name,age in children.items():
+        print(f"Name:{name},Age:{age}")
+def if_John(children):
+    if 'John' in children:
+        print('yes')
+    else:
+        print('No')
+def main():
+    children={}
+    input_dic(children)
+    print_dic(children)
+    if_John(children)
+main()
